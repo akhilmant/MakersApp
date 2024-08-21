d21a930fa41b0a6afa27f290806f3027f3df8c17
akhil-19050 <akhil.mant@zohocorp.com>
2024-08-21 23:45:15 +0530

    Initial Commit

---
+++ MakersApp/MakersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakersAppApp.swift
+//  MakersApp
+//
+//  Created by akhil-19050 on 21/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MakersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
